18e2af7fef26418e719f1a5c93050c9fe0458dc5
Micahaha <xicer029@gmail.com>
2025-01-29 09:09:47 -0500

    Order Details

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
-
+import { Card, Descriptions, Divider, List, Button } from 'antd';
+import dishes from '../src/assets/data/dishes.json'
 function App() {
   return (
-    <div>
-    <span>Hello</span>
-    </div>
+    <Card title ='Order Title' style={{margin: 20}}>
+      <span> Hello </span>
+    </Card>
 
   
     
