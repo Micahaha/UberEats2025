37be77607b56abfbdb5667eae198b74e3271d25b
Micahaha <xicer029@gmail.com>
2025-02-05 11:34:56 -0500

    Ubereats2025

--- src/App.js
+++ src/App.js
@@ -4,11 +4,26 @@ import dishes from '../src/assets/data/dishes.json'
 function App() {
   return (
     <Card title ='Order Title' style={{margin: 20}}>
-      <span> Hello </span>
-    </Card>
-
-  
-    
+      <Descriptions bordered column={{lg: 1, md: 1, sm: 1}}>
+        <Descriptions.Item label="Customer">Lukas Grinevicius</Descriptions.Item>
+        <Descriptions.Item label="Customer Address">Address 15-29 City, Country</Descriptions.Item>
+      </Descriptions>
+      <Divider/>
+        <List
+        dataSource={dishes}
+        renderItem={(dishItem) => (
+          <List.Item>
+            <div style={{fontWeight: 'bold'}}>{dishItem.name} x{dishItem.quantity}</div>
+            <div>${dishItem.price}</div>
+          </List.Item>
+        )}
+        /> 
+        <Divider />
+        <div>
+          <h2>Total: </h2>
+          <h2>$42.96</h2>
+        </div>
+    </Card> 
   );
 }
 
